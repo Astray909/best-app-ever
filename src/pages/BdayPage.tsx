8153e4140c25e03577ec9868597f36ba60a972e5
Justin Huang <justin9.ij@gmail.com>
2026-04-18 23:36:21 -0400

    jhuang feat: Update birthday page with new styling and enhanced functionality - Changed dialog background color for better visibility - Added interactive name display with toggle functionality - Updated birthday message with personalized text - Adjusted CSS for improved layout and responsiveness

--- src/pages/BdayPage.tsx
+++ src/pages/BdayPage.tsx
@@ -4,6 +4,7 @@ import './BdayPage.css'
 
 export default function BdayPage() {
   const [open, setOpen] = useState(false)
+  const [pookie, setPookie] = useState(false)
 
   return (
     <div className="bday-page">
@@ -15,8 +16,26 @@ export default function BdayPage() {
       {open && (
         <div className="bday-overlay" onClick={() => setOpen(false)}>
           <div className="bday-dialog" onClick={e => e.stopPropagation()}>
-            <p className="bday-message">Happy Birthday! 🎂</p>
-            <p className="bday-note"></p>
+            <p className="bday-message">
+              Happy Birthday! My{' '}
+              <span className="bday-name" onClick={() => setPookie(p => !p)}>
+                {pookie ? 'Pookie 🤫' : 'Honey 🎂'}
+              </span>
+            </p>
+            <p className="bday-note">
+              Happy 29th birthday to my favourite person in the world!
+              <br />
+              Now that you are turning 19 for the 10th time,
+              we will have to wait until September for our age gap to get back to 2.
+              <br />
+              I hope this year has been treating you well so far (it should, because I appeared in it 😎) 
+              and that your 29th time on this planet revolving our sun will be the best one yet!
+              <br />
+              I am looking forward to all the adventures we will have together. Even though you are the birthday
+              girl, I almost feel like I was the one who got the best present in the universe when I met you.
+              <br />
+              Happy Birthday!!!!! And I hope you are having the best day ever! 🎉🎁🎈
+            </p>
             <button className="bday-close" onClick={() => setOpen(false)}>Close</button>
           </div>
         </div>
